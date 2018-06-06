e46ea02ef7af29e7e2bed4e745f5516d0bac3024
Kevin Žiher <owner312@gmail.com>
2018-06-06 11:55:47 +0200

    Dodajam login da bo frank srečn

--- src/si/um/feri/praktikum/controler/LoginControler.java
+++ src/si/um/feri/praktikum/controler/LoginControler.java
@@ -24,7 +24,17 @@ public class LoginControler {
 	MongoClientProvider mongoClientProvider = MongoClientProvider.getInstance();
 	MongoClient mongoClient = mongoClientProvider.getMongo();
 	
+	public String getEmail() {
+		return email;}
 	
+	public void setEmail(String email) {
+		this.email = email;}
+
+	public String getPassword() {
+		return password;}
+
+	public void setPassword(String password) {
+		this.password = password;}
 	
 	
 	

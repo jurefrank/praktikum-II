5d6198d6a2c8f5568d285783da8b73272a162d26
Jakob Cerjak <jakob.cerjak@student.um.si>
2018-06-14 19:27:27 +0200

    Sfiniširan izpis PDF-ja in malo popravkov v CSS

--- src/si/um/feri/praktikum/controler/PDFController.java
+++ src/si/um/feri/praktikum/controler/PDFController.java
@@ -8,6 +8,7 @@ import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletResponse;
 
 import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
@@ -23,8 +24,8 @@ import si.um.feri.praktikum.entity.Record;
 @SessionScoped
 public class PDFController {
 	
-	 private static Font mainTitle = new Font(Font.FontFamily.HELVETICA, 24, Font.BOLD, BaseColor.BLACK);
-	 private static Font tekstFont = new Font(Font.FontFamily.HELVETICA, 12, Font.NORMAL, BaseColor.BLACK);
+	 private static Font mainTitle = new Font(Font.FontFamily.TIMES_ROMAN, 24, Font.BOLD, BaseColor.BLUE);
+	 private static Font tekstFont = new Font(Font.FontFamily.TIMES_ROMAN, 14, Font.NORMAL, BaseColor.BLACK);
 	 
 	
 	public void createPDF(Record record) throws DocumentException, IOException {
@@ -45,29 +46,35 @@ public class PDFController {
 	        document.addCreationDate();
 	        document.addTitle("Document with information about records and their data.");
 	        document.open();
-	        
+	     
 	        LineSeparator lineSeparator = new LineSeparator();
 	        lineSeparator.setOffset(-3);
 	        
-	        Paragraph mainTitle = new Paragraph(record.getTitle() + " " + "Version " + record.getVersion());
+	        Paragraph mainTitle = new Paragraph(record.getTitle() + " - " + "Version " + record.getVersion());
 	        mainTitle.setFont(PDFController.mainTitle);
 	        document.add(mainTitle);
 	        
 	        Paragraph recordKeeper = new Paragraph("Record keeper information ");
 	        	recordKeeper.add(lineSeparator);
-	        	Phrase phrase = new Phrase("Name:" + record.getNameManager() + "his contact data is : " 
-	        								+ record.getEmailManager() + "phone number " 
+	        	Phrase phrase = new Phrase("Name : " + record.getNameManager()
+	        	+ System.lineSeparator()+ "His contact is : " 
+	        								+ record.getEmailManager() +System.lineSeparator()+ "Phone number : " 
 	        								+ record.getPhoneNumberManager());
-	        	
-	        	
+	        	recordKeeper.setFont(PDFController.mainTitle);
+	        	phrase.setFont(tekstFont);
+	        
 	        document.add(recordKeeper);
 	        document.add(phrase);
 	        
 	        Paragraph storingData = new Paragraph("Storing data about");
 	        		storingData.add(lineSeparator);
 	        		
-	        Phrase phrase2 = new Phrase( "Categories: " + record.getCategoriesOfUsersWhomPersonalDataDisclosed());
+	        Phrase phrase2 = new Phrase( "Categories : " + record.getCategoriesOfUsersWhomPersonalDataDisclosed());
+	        phrase2.add(Chunk.NEWLINE);
 	        Phrase phrase3 = new Phrase("Which user data is being transfered: " + record.getInformationOnTransfersOfPersonalData());
+	        
+	        phrase2.setFont(tekstFont);
+	        phrase3.setFont(tekstFont);
 	        
 	        
 	        document.add(storingData);
